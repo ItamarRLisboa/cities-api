1f07ca17168b8979779c467fc5bdec593ca431ea
ItamarRLisboa <itamar.lisboa@gmail.com>
2021-05-23 21:36:45 -0300

    Primeiro Commit

---
+++ src/main/java/com/github/ItamarRLisboa/citiesapi/staties/resources/StateResource.java
@@ -0,0 +1,24 @@
+package com.github.ItamarRLisboa.citiesapi.staties.resources;
+
+import com.github.ItamarRLisboa.citiesapi.staties.entities.State;
+import com.github.ItamarRLisboa.citiesapi.staties.repositories.StateRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/states")
+public class StateResource {
+
+    private final StateRepository repository;
+
+    public StateResource(final StateRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<State> states() {
+        return repository.findAll();
+    }
+}
